20ef35b573c79d6f78357b9a1d5fd1702cf56129
telamonian <telamonian@users.noreply.github.com>
2026-01-08 18:34:08 -0500

    init_board script now working

--- init_board.gd
+++ init_board.gd
@@ -1,16 +1,24 @@
 extends TileMapLayer
 
-const DARK_SQUARE = Vector2i(0, 1)
+const DARK_SQUARE = Vector2i(0, 0)
+const DARK_SQUARE_SOURCE = 0
 const LIGHT_SQUARE = Vector2i(0, 0)
+const LIGHT_SQUARE_SOURCE = 1
 
 func init_board():
-	for i in range(8):
-		for j in range(8):
+	for file in range(8):
+		for row in range(8):
+			var parity = (file % 2 + row % 2) % 2
+			var square = LIGHT_SQUARE if parity else DARK_SQUARE
+			var source = LIGHT_SQUARE_SOURCE if parity else DARK_SQUARE_SOURCE
 			
+			var inverse_row = 7 - row
+			# use inverse row to paint the squares bottom-to-top
+			set_cell(Vector2(file, inverse_row), source, square)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	init_board()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
