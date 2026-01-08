29d80d3730533c29365b2a6a74bcd0e95290d176
telamonian <telamonian@users.noreply.github.com>
2026-01-08 17:47:51 -0500

    initial commit

---
+++ init_board.gd
@@ -0,0 +1,18 @@
+extends TileMapLayer
+
+const DARK_SQUARE = Vector2i(0, 1)
+const LIGHT_SQUARE = Vector2i(0, 0)
+
+func init_board():
+	for i in range(8):
+		for j in range(8):
+			
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
