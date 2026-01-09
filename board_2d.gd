4d2af641c14d2d1ae73fdcd27271762e3f8445d0
telamonian <telamonian@users.noreply.github.com>
2026-01-08 22:22:54 -0500

    board is now fully populated with standard pieces

--- board_2d.gd
+++ board_2d.gd
@@ -14,12 +14,40 @@ func global_to_grid(global_pos: Vector2) -> Vector2i:
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+  tmlayer.init_board()
+
+  # rooks
+  man.spawn_piece(Enum.Pcolor.WHITE, Enum.Ptype.ROOK, Vector2i(0, 0))
+  man.spawn_piece(Enum.Pcolor.WHITE, Enum.Ptype.ROOK, Vector2i(7, 0))
   
+  man.spawn_piece(Enum.Pcolor.BLACK, Enum.Ptype.ROOK, Vector2i(7, 7))
+  man.spawn_piece(Enum.Pcolor.BLACK, Enum.Ptype.ROOK, Vector2i(0, 7))
+  
+  # knights
+  man.spawn_piece(Enum.Pcolor.WHITE, Enum.Ptype.KNIGHT, Vector2i(1, 0))
+  man.spawn_piece(Enum.Pcolor.WHITE, Enum.Ptype.KNIGHT, Vector2i(6, 0))
+  
+  man.spawn_piece(Enum.Pcolor.BLACK, Enum.Ptype.KNIGHT, Vector2i(1, 7))
+  man.spawn_piece(Enum.Pcolor.BLACK, Enum.Ptype.KNIGHT, Vector2i(6, 7))
+  
+  # bishops
+  man.spawn_piece(Enum.Pcolor.WHITE, Enum.Ptype.BISHOP, Vector2i(2, 0))
+  man.spawn_piece(Enum.Pcolor.WHITE, Enum.Ptype.BISHOP, Vector2i(5, 0))
+  
+  man.spawn_piece(Enum.Pcolor.BLACK, Enum.Ptype.BISHOP, Vector2i(2, 7))
+  man.spawn_piece(Enum.Pcolor.BLACK, Enum.Ptype.BISHOP, Vector2i(5, 7))
+  
+  # royals
+  man.spawn_piece(Enum.Pcolor.WHITE, Enum.Ptype.QUEEN, Vector2i(3, 0))
+  man.spawn_piece(Enum.Pcolor.WHITE, Enum.Ptype.KING, Vector2i(4, 0))
+  
+  man.spawn_piece(Enum.Pcolor.BLACK, Enum.Ptype.QUEEN, Vector2i(3, 7))
+  man.spawn_piece(Enum.Pcolor.BLACK, Enum.Ptype.KING, Vector2i(4, 7))
+    
   for file in range(8):
-    man.spawn_piece(Enum.Pcolor.WHITE, Enum.Ptype.PAWN, Vector2i(file, 1))
+    man.spawn_piece(Enum.Pcolor.WHITE, Enum.Ptype.PAWN, Vector2i(file, 1))    
     man.spawn_piece(Enum.Pcolor.BLACK, Enum.Ptype.PAWN, Vector2i(file, 6))
   
-  tmlayer.init_board()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
