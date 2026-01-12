21c3996de351865cb70d92ab68fccdaf95706f55
telamonian <telamonian@users.noreply.github.com>
2026-01-12 04:06:25 -0500

    added move generators for pawn, knight, king, and rook

--- game/player/player_2d.gd
+++ game/player/player_2d.gd
@@ -8,6 +8,7 @@ var is_in_check: bool = false
 var row_back: int
 var row_front: int
 var row_promote: int
+var pawn_dir: int
 
 #var pieces: Dictionary[Vector2i, Piece2D] = {}
 
@@ -20,10 +21,64 @@ func setup(new_id: int, new_color: Enum.Pcolor):
     row_back = 0
     row_front = 1
     row_promote = 7
+    pawn_dir = 1
   elif color == Enum.Pcolor.BLACK:
     row_back = 7
     row_front = 6
     row_promote = 0
+    pawn_dir = -1
+
+func moves_pawn(grid_pos: Vector2i) -> Array[Vector2i]:
+  var file = grid_pos.x
+  var row = grid_pos.y
+  return [
+    Vector2i(file, row + 1*pawn_dir),
+    Vector2i(file + 1, row + 1*pawn_dir),
+    Vector2i(file - 1, row + 1*pawn_dir),
+    Vector2i(file, row + 2*pawn_dir)
+  ]
+
+func moves_knight(grid_pos: Vector2i) -> Array[Vector2i]:
+  var file = grid_pos.x
+  var row = grid_pos.y
+  var moves: Array[Vector2i] = []
+
+  for i in [-2, 2]:
+    for j in [-1, 1]:
+      moves.append(Vector2i(file + i, row + j))
+      moves.append(Vector2i(file + j, row + i))
+
+  return moves
+
+func moves_king(grid_pos: Vector2i) -> Array[Vector2i]:
+  var file = grid_pos.x
+  var row = grid_pos.y
+  var moves: Array[Vector2i] = []
+
+  for i in [-1, 0, 1]:
+    for j in [-1, 0, 1]:
+      if i != 0 or j != 0:
+        moves.append(Vector2i(file + i, row + j))
+
+  return moves
+
+func moves_rook(grid_pos: Vector2i) -> Array[Array]:
+  var file = grid_pos.x
+  var row = grid_pos.y
+  var moves: Array[Array] = []
+
+  for i in [-1, 1]:
+    var moves_file: Array[Vector2i] = []
+    var moves_row: Array[Vector2i] = []
+
+    for j in range(1, 8):
+        moves_file.append(Vector2i(file, row + i*j))
+        moves_row.append(Vector2i(file + i*j, row))
+
+    moves.append(moves_file)
+    moves.append(moves_row)
+
+  return moves
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
