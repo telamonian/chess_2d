389f12dde58ef780bb1e03831d1242a6c90ae00c
telamonian <telamonian@users.noreply.github.com>
2026-01-30 00:47:59 -0500

    fixed issue with settings menu layout and setting button overlap
    - can actually repeatably open and close settings menu now

--- game/settings_button.gd
+++ game/settings_button.gd
@@ -6,9 +6,9 @@ var menu
 var menu_hidden = true:
   set(x):
     if not menu_hidden and x == true:
-      get_parent().remove_child(menu)
+      get_tree().root.remove_child(menu)
     elif menu_hidden and x == false:
-      get_parent().add_child(menu)
+      get_tree().root.add_child(menu)
 
     menu_hidden = x
 
