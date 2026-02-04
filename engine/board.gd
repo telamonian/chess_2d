a1a911a09b0671a3281ad21998bedb7ea67f913b
telamonian <telamonian@users.noreply.github.com>
2026-02-04 05:19:55 -0500

    added board to engine

---
+++ engine/board.gd
@@ -0,0 +1,11 @@
+class_name Board extends Object
+
+var FILES: int
+var ROWS: int
+
+func spawn_board(files: int, rows: int):
+  FILES = files
+  ROWS = rows
+
+func is_inbounds(grid_pos: Vector2i) -> bool:
+  return 0 <= grid_pos.x and grid_pos.x < FILES and 0 <= grid_pos.y and grid_pos.y < ROWS
