65a10ff3590a1a8f67ccb2d5a2f8ba2272fceb82
telamonian <telamonian@users.noreply.github.com>
2026-02-04 03:29:27 -0500

    started refactoring out all business logic to simple objects in engine

---
+++ engine/piece.gd
@@ -0,0 +1,105 @@
+class_name Piece extends Object
+
+var player_id: int
+var pawn_dir: int
+var color: Enum.Pcolor
+var type: Enum.Ptype
+var grid_position: Vector2i
+
+var is_moved: bool = false
+
+var file:
+  get():
+    return grid_position.x
+var row:
+  get():
+    return grid_position.y
+
+func _init(player_id: int, pawn_dir: int, color: Enum.Pcolor, type: Enum.Ptype, grid_pos: Vector2i):
+  setup(player_id, pawn_dir, color, type, grid_pos)
+
+func move(grid_pos: Vector2i):
+  grid_position = grid_pos
+  is_moved = true
+
+func setup(pid: int, pdir: int, c: Enum.Pcolor, t: Enum.Ptype, grid_pos: Vector2i):
+  player_id = pid
+  pawn_dir = pdir
+  color = c
+  type = t
+  grid_position = grid_pos
+
+func get_moves():
+  match type:
+    Enum.Ptype.KING:
+      return moves_king(file, row)
+    Enum.Ptype.QUEEN:
+      return moves_queen(file, row)
+    Enum.Ptype.BISHOP:
+      return moves_bishop(file, row)
+    Enum.Ptype.KNIGHT:
+      return moves_knight(file, row)
+    Enum.Ptype.ROOK:
+      return moves_rook(file, row)
+    Enum.Ptype.PAWN:
+      return moves_pawn(file, row, pawn_dir)
+
+static func moves_pawn(file: int, row: int, pawn_dir: int) -> Array[Vector2i]:
+  return [Vector2i(file, row + 1*pawn_dir)]
+
+static func moves_knight(file: int, row: int) -> Array[Vector2i]:
+  var moves: Array[Vector2i] = []
+
+  for i in [-2, 2]:
+    for j in [-1, 1]:
+      moves.append(Vector2i(file + i, row + j))
+      moves.append(Vector2i(file + j, row + i))
+
+  return moves
+
+static func moves_king(file: int, row: int) -> Array[Vector2i]:
+  var moves: Array[Vector2i] = []
+
+  for i in [-1, 0, 1]:
+    for j in [-1, 0, 1]:
+      if i != 0 or j != 0:
+        moves.append(Vector2i(file + i, row + j))
+
+  return moves
+
+static func moves_rook(file: int, row: int) -> Array[Array]:
+  var moves: Array[Array] = []
+
+  for i in [-1, 1]:
+    var moves_file: Array[Vector2i] = []
+    var moves_row: Array[Vector2i] = []
+
+    for j in range(1, 8):
+      moves_file.append(Vector2i(file, row + i*j))
+      moves_row.append(Vector2i(file + i*j, row))
+
+    moves.append(moves_file)
+    moves.append(moves_row)
+
+  return moves
+
+static func moves_bishop(file: int, row: int) -> Array[Array]:
+  var moves: Array[Array] = []
+
+  for i in [-1, 1]:
+    var moves_positive_diagonal: Array[Vector2i] = []
+    var moves_negative_diagonal: Array[Vector2i] = []
+
+    for j in range(1, 8):
+      moves_positive_diagonal.append(Vector2i(file + i*j, row + i*j))
+      moves_negative_diagonal.append(Vector2i(file - i*j, row + i*j))
+
+    moves.append(moves_positive_diagonal)
+    moves.append(moves_negative_diagonal)
+
+  return moves
+
+static func moves_queen(file: int, row: int) -> Array[Array]:
+  var moves = moves_rook(file, row)
+  moves.append_array(moves_bishop(file, row))
+  return moves
---
+++ engine/piece_manager.gd
@@ -0,0 +1,62 @@
+class_name PieceManager extends Object
+
+var pieces: Dictionary[Vector2i, Piece] = {}
+var kings: Dictionary[int, Piece] = {}
+
+func move_piece(grid_pos: Vector2i, new_grid_pos: Vector2i) -> void:
+  var piece = pieces[grid_pos]
+
+  if grid_pos != new_grid_pos:
+    # piece has actually moved
+    if new_grid_pos in pieces:
+      # remove any pre-existing piece in new location
+      remove_piece(new_grid_pos)
+
+    pieces.erase(grid_pos)
+    piece.move(new_grid_pos)
+    #piece.move(new_grid_pos, game.board.grid_to_local(new_grid_pos))
+    pieces[new_grid_pos] = piece
+
+func remove_piece(grid_pos: Vector2i):
+  var piece: Piece = pieces.get(grid_pos)
+
+  pieces.erase(grid_pos)
+  if piece.type == Enum.Ptype.KING:
+    kings.erase(piece.player_id)
+  #remove_child(piece)
+  piece.queue_free()
+
+func spawn_piece(player_id: int, pawn_dir: int, color: Enum.Pcolor, type: Enum.Ptype, grid_pos: Vector2i) -> Piece:
+  var piece = Piece.new(player_id, pawn_dir, color, type, grid_pos)
+
+  #add_child(piece)
+  pieces[grid_pos] = piece
+
+  return piece
+
+func spawn_back(row: int, player_id: int, pawn_dir: int, color: Enum.Pcolor):
+  # rooks
+  for i in [0, 7]:
+    spawn_piece(player_id, pawn_dir, color, Enum.Ptype.ROOK, Vector2i(i, row))
+
+  # knights
+  for i in [1, 6]:
+    spawn_piece(player_id, pawn_dir, color, Enum.Ptype.KNIGHT, Vector2i(i, row))
+
+  # bishops
+  for i in [2, 5]:
+    spawn_piece(player_id, pawn_dir, color, Enum.Ptype.BISHOP, Vector2i(i, row))
+
+  # royals
+  spawn_piece(player_id, pawn_dir, color, Enum.Ptype.QUEEN, Vector2i(3, row))
+  var king = spawn_piece(player_id, pawn_dir, color, Enum.Ptype.KING, Vector2i(4, row))
+  kings[player_id] = king
+
+func spawn_front(row: int, player_id: int, pawn_dir: int, color: Enum.Pcolor):
+  # pawns
+  for i in range(8):
+    spawn_piece(player_id, pawn_dir, color, Enum.Ptype.PAWN, Vector2i(i, row))
+
+func spawn_pieces_for_player(player: Player2D):
+  spawn_back(player.row_back, player.id, player.pawn_dir, player.color)
+  spawn_front(player.row_front, player.id, player.pawn_dir, player.color)
